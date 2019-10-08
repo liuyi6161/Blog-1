403da1c1e8f7186c3910f20a720aafd11841e426
zsw <zsw0407@outlook.com>
2019-10-08 14:08:24 +0800

    更新依赖和注释无关配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  theme: 'reco',
+  // theme: 'reco',
   themeConfig: {
     type: 'blog',
     huawei: false,
