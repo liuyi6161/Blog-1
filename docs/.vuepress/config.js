6b02c91d4c826193668ea0785588edb48f3598f5
zsw <zsw0407@outlook.com>
2019-10-09 11:00:59 +0800

    JS数据类型的笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,7 +49,7 @@ module.exports = {
       '/note/': [
         {
           title: 'HTML5',
-          collapsable: false,
+          collapsable: true,
           children: [
             'html5/HTML5的语义元素',
             'html5/HTML5多媒体标签',
@@ -60,15 +60,22 @@ module.exports = {
         },
         {
           title: 'CSS',
-          collapsable: false,
+          collapsable: true,
           children: [
             'css/css3新特性',
             'css/css3Flip'
           ]
         },
         {
+          title: 'JS',
+          collapsable: true,
+          children: [
+            'js/js数据类型'
+          ]
+        },
+        {
           title: '前端单元测试',
-          collapsable: false,
+          collapsable: true,
           children: [
             'fe-unit-test/chai',
             'fe-unit-test/mocha',
